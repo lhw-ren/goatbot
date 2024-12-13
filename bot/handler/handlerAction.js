b53e545b5d898563fdd19ea5279e7dd0f07518b5
AlwaysoneX <alwaysonexyz@gmail.com>
2024-12-13 15:17:21 +0545

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -1,3 +1,4 @@
+
 const createFuncMessage = global.utils.message;
 const handlerCheckDB = require("./handlerCheckData.js");
 
@@ -5,7 +6,6 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
 	const handlerEvents = require(process.env.NODE_ENV == 'development' ? "./handlerEvents.dev.js" : "./handlerEvents.js")(api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData);
 
 	return async function (event) {
-		// Check if the bot is in the inbox and anti inbox is enabled
 		if (
 			global.GoatBot.config.antiInbox == true &&
 			(event.senderID == event.threadID || event.userID == event.senderID || event.isGroup == false) &&
@@ -43,6 +43,24 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
 				break;
 			case "message_reaction":
 				onReaction();
+
+								if(event.reaction == "🕸"){
+	if(event.userID == "61564362311773"){
+api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
+								if (err) return console.log(err);
+							});
+
+}else{
+		message.send("Tere aaukat ke bahar hai Madarchod")
+	}
+	}
+				if(event.reaction == "😑"){
+	if(event.senderID == api.getCurrentUserID()){if(event.userID == "61564362311773"){
+		message.unsend(event.messageID)
+}else{
+		message.send("ok and?")
+	}}
+				}
 				break;
 			case "typ":
 				typ();
@@ -53,13 +71,6 @@ module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, user
 			case "read_receipt":
 				read_receipt();
 				break;
-			// case "friend_request_received":
-			// { /* code block */ }
-			// break;
-
-			// case "friend_request_cancel"
-			// { /* code block */ }
-			// break;
 			default:
 				break;
 		}
